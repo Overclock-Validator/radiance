d2fd1dae9143808371cf7f8ad7b30c81d98ff5d0
smcio <shaun@darkwaves.io>
2024-07-15 14:29:32 +0100

    sealevel test: system program Transfer instr testcases

--- pkg/sealevel/sealevel_system_program_test.go
+++ pkg/sealevel/sealevel_system_program_test.go
@@ -600,3 +600,239 @@ func TestExecute_Tx_System_Program_Assign_Not_Signer_Failure(t *testing.T) {
 	err = execCtx.ProcessInstruction(instrBytes, instructionAccts, []uint64{0})
 	assert.Equal(t, InstrErrMissingRequiredSignature, err)
 }
+
+func TestExecute_Tx_System_Program_Transfer_Success(t *testing.T) {
+
+	// system program acct
+	systemProgramAcct := accounts.Account{Key: SystemProgramAddr, Lamports: 100000000, Data: make([]byte, 0), Owner: NativeLoaderAddr, Executable: true, RentEpoch: 100}
+
+	// funding acct
+	fundingAcctPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	fundingPubkey := fundingAcctPrivateKey.PublicKey()
+	fundingAcct := accounts.Account{Key: fundingPubkey, Lamports: 10000, Data: make([]byte, 0), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	// recipient acct
+	recipientPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	recipientPubkey := recipientPrivateKey.PublicKey()
+	recipientAcct := accounts.Account{Key: recipientPubkey, Lamports: 0, Data: make([]byte, 0), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	var transfer SystemInstrTransfer
+	transfer.Lamports = 1337
+
+	transferInstrWriter := new(bytes.Buffer)
+	transferEncoder := bin.NewBinEncoder(transferInstrWriter)
+
+	err = transfer.MarshalWithEncoder(transferEncoder)
+	assert.NoError(t, err)
+	instrBytes := transferInstrWriter.Bytes()
+
+	transactionAccts := NewTransactionAccounts([]accounts.Account{systemProgramAcct, fundingAcct, recipientAcct})
+
+	acctMetas := []AccountMeta{{Pubkey: fundingAcct.Key, IsSigner: true, IsWritable: true},
+		{Pubkey: recipientAcct.Key, IsSigner: false, IsWritable: true}}
+
+	instructionAccts := instructionAcctsFromAccountMetas(acctMetas, *transactionAccts)
+
+	txCtx := NewTestTransactionCtx(*transactionAccts, 5, 64)
+	execCtx := ExecutionCtx{TransactionContext: txCtx, ComputeMeter: cu.NewComputeMeter(10000000000)}
+
+	execCtx.Accounts = accounts.NewMemAccounts()
+	var clock SysvarClock
+	clock.Slot = 1234
+	clockAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarClockAddr, &clockAcct)
+	WriteClockSysvar(&execCtx.Accounts, clock)
+
+	var rent SysvarRent
+	rent.LamportsPerUint8Year = 1
+	rent.ExemptionThreshold = 1
+	rent.BurnPercent = 0
+
+	rentAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarRentAddr, &rentAcct)
+	WriteRentSysvar(&execCtx.Accounts, rent)
+
+	err = execCtx.ProcessInstruction(instrBytes, instructionAccts, []uint64{0})
+	assert.NoError(t, err)
+
+	fundingAcctPost, err := txCtx.Accounts.GetAccount(1)
+	assert.NoError(t, err)
+	assert.Equal(t, fundingAcct.Lamports-transfer.Lamports, fundingAcctPost.Lamports)
+
+	recipientAcctPost, err := txCtx.Accounts.GetAccount(2)
+	assert.NoError(t, err)
+	assert.Equal(t, transfer.Lamports, recipientAcctPost.Lamports)
+}
+
+func TestExecute_Tx_System_Program_Transfer_From_Not_Signer_Failure(t *testing.T) {
+
+	// system program acct
+	systemProgramAcct := accounts.Account{Key: SystemProgramAddr, Lamports: 100000000, Data: make([]byte, 0), Owner: NativeLoaderAddr, Executable: true, RentEpoch: 100}
+
+	// funding acct
+	fundingAcctPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	fundingPubkey := fundingAcctPrivateKey.PublicKey()
+	fundingAcct := accounts.Account{Key: fundingPubkey, Lamports: 10000, Data: make([]byte, 0), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	// recipient acct
+	recipientPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	recipientPubkey := recipientPrivateKey.PublicKey()
+	recipientAcct := accounts.Account{Key: recipientPubkey, Lamports: 0, Data: make([]byte, 0), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	var transfer SystemInstrTransfer
+	transfer.Lamports = 1337
+
+	transferInstrWriter := new(bytes.Buffer)
+	transferEncoder := bin.NewBinEncoder(transferInstrWriter)
+
+	err = transfer.MarshalWithEncoder(transferEncoder)
+	assert.NoError(t, err)
+	instrBytes := transferInstrWriter.Bytes()
+
+	transactionAccts := NewTransactionAccounts([]accounts.Account{systemProgramAcct, fundingAcct, recipientAcct})
+
+	acctMetas := []AccountMeta{{Pubkey: fundingAcct.Key, IsSigner: false, IsWritable: true},
+		{Pubkey: recipientAcct.Key, IsSigner: false, IsWritable: true}}
+
+	instructionAccts := instructionAcctsFromAccountMetas(acctMetas, *transactionAccts)
+
+	txCtx := NewTestTransactionCtx(*transactionAccts, 5, 64)
+	execCtx := ExecutionCtx{TransactionContext: txCtx, ComputeMeter: cu.NewComputeMeter(10000000000)}
+
+	execCtx.Accounts = accounts.NewMemAccounts()
+	var clock SysvarClock
+	clock.Slot = 1234
+	clockAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarClockAddr, &clockAcct)
+	WriteClockSysvar(&execCtx.Accounts, clock)
+
+	var rent SysvarRent
+	rent.LamportsPerUint8Year = 1
+	rent.ExemptionThreshold = 1
+	rent.BurnPercent = 0
+
+	rentAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarRentAddr, &rentAcct)
+	WriteRentSysvar(&execCtx.Accounts, rent)
+
+	err = execCtx.ProcessInstruction(instrBytes, instructionAccts, []uint64{0})
+	assert.Equal(t, InstrErrMissingRequiredSignature, err)
+}
+
+func TestExecute_Tx_System_Program_Transfer_From_Has_Data_Failure(t *testing.T) {
+
+	// system program acct
+	systemProgramAcct := accounts.Account{Key: SystemProgramAddr, Lamports: 100000000, Data: make([]byte, 0), Owner: NativeLoaderAddr, Executable: true, RentEpoch: 100}
+
+	// funding acct
+	fundingAcctPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	fundingPubkey := fundingAcctPrivateKey.PublicKey()
+	fundingAcct := accounts.Account{Key: fundingPubkey, Lamports: 10000, Data: make([]byte, 100), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	// recipient acct
+	recipientPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	recipientPubkey := recipientPrivateKey.PublicKey()
+	recipientAcct := accounts.Account{Key: recipientPubkey, Lamports: 0, Data: make([]byte, 0), Owner: BpfLoaderUpgradeableAddr, Executable: false, RentEpoch: 100}
+
+	var transfer SystemInstrTransfer
+	transfer.Lamports = 1337
+
+	transferInstrWriter := new(bytes.Buffer)
+	transferEncoder := bin.NewBinEncoder(transferInstrWriter)
+
+	err = transfer.MarshalWithEncoder(transferEncoder)
+	assert.NoError(t, err)
+	instrBytes := transferInstrWriter.Bytes()
+
+	transactionAccts := NewTransactionAccounts([]accounts.Account{systemProgramAcct, fundingAcct, recipientAcct})
+
+	acctMetas := []AccountMeta{{Pubkey: fundingAcct.Key, IsSigner: true, IsWritable: true},
+		{Pubkey: recipientAcct.Key, IsSigner: false, IsWritable: true}}
+
+	instructionAccts := instructionAcctsFromAccountMetas(acctMetas, *transactionAccts)
+
+	txCtx := NewTestTransactionCtx(*transactionAccts, 5, 64)
+	execCtx := ExecutionCtx{TransactionContext: txCtx, ComputeMeter: cu.NewComputeMeter(10000000000)}
+
+	execCtx.Accounts = accounts.NewMemAccounts()
+	var clock SysvarClock
+	clock.Slot = 1234
+	clockAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarClockAddr, &clockAcct)
+	WriteClockSysvar(&execCtx.Accounts, clock)
+
+	var rent SysvarRent
+	rent.LamportsPerUint8Year = 1
+	rent.ExemptionThreshold = 1
+	rent.BurnPercent = 0
+
+	rentAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarRentAddr, &rentAcct)
+	WriteRentSysvar(&execCtx.Accounts, rent)
+
+	err = execCtx.ProcessInstruction(instrBytes, instructionAccts, []uint64{0})
+	assert.Equal(t, InstrErrInvalidArgument, err)
+}
+
+func TestExecute_Tx_System_Program_Transfer_Not_Enough_Lamports_In_From_Acct(t *testing.T) {
+
+	// system program acct
+	systemProgramAcct := accounts.Account{Key: SystemProgramAddr, Lamports: 100000000, Data: make([]byte, 0), Owner: NativeLoaderAddr, Executable: true, RentEpoch: 100}
+
+	// funding acct
+	fundingAcctPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	fundingPubkey := fundingAcctPrivateKey.PublicKey()
+	fundingAcct := accounts.Account{Key: fundingPubkey, Lamports: 100, Data: make([]byte, 0), Owner: SystemProgramAddr, Executable: false, RentEpoch: 100}
+
+	// recipient acct
+	recipientPrivateKey, err := solana.NewRandomPrivateKey()
+	assert.NoError(t, err)
+	recipientPubkey := recipientPrivateKey.PublicKey()
+	recipientAcct := accounts.Account{Key: recipientPubkey, Lamports: 0, Data: make([]byte, 0), Owner: BpfLoaderUpgradeableAddr, Executable: false, RentEpoch: 100}
+
+	var transfer SystemInstrTransfer
+	transfer.Lamports = 1000000
+
+	transferInstrWriter := new(bytes.Buffer)
+	transferEncoder := bin.NewBinEncoder(transferInstrWriter)
+
+	err = transfer.MarshalWithEncoder(transferEncoder)
+	assert.NoError(t, err)
+	instrBytes := transferInstrWriter.Bytes()
+
+	transactionAccts := NewTransactionAccounts([]accounts.Account{systemProgramAcct, fundingAcct, recipientAcct})
+
+	acctMetas := []AccountMeta{{Pubkey: fundingAcct.Key, IsSigner: true, IsWritable: true},
+		{Pubkey: recipientAcct.Key, IsSigner: false, IsWritable: true}}
+
+	instructionAccts := instructionAcctsFromAccountMetas(acctMetas, *transactionAccts)
+
+	txCtx := NewTestTransactionCtx(*transactionAccts, 5, 64)
+	execCtx := ExecutionCtx{TransactionContext: txCtx, ComputeMeter: cu.NewComputeMeter(10000000000)}
+
+	execCtx.Accounts = accounts.NewMemAccounts()
+	var clock SysvarClock
+	clock.Slot = 1234
+	clockAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarClockAddr, &clockAcct)
+	WriteClockSysvar(&execCtx.Accounts, clock)
+
+	var rent SysvarRent
+	rent.LamportsPerUint8Year = 1
+	rent.ExemptionThreshold = 1
+	rent.BurnPercent = 0
+
+	rentAcct := accounts.Account{}
+	execCtx.Accounts.SetAccount(&SysvarRentAddr, &rentAcct)
+	WriteRentSysvar(&execCtx.Accounts, rent)
+
+	err = execCtx.ProcessInstruction(instrBytes, instructionAccts, []uint64{0})
+	assert.Equal(t, SystemProgErrResultWithNegativeLamports, err)
+}
